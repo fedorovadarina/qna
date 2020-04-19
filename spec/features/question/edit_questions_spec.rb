e6d113c6b5135712298895cd58c012bfe602740d
darina <darinafedorova1997@gmail.com>
2020-04-19 16:42:38 +0300

    attach files when edit question

--- spec/features/question/edit_questions_spec.rb
+++ spec/features/question/edit_questions_spec.rb
@@ -44,6 +44,14 @@ feature 'User can edit his question', %q{
       expect(page).to have_content question.title
       expect(page).to have_content question.body
     end
+
+    scenario 'adding attachment' do
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb","#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Update Question'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Unauthenticated user cannot edit a question' do
